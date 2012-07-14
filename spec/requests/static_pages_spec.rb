38408ca1181328559d40132a6374ca361d034918
John-Henry Liberty <johnhenry.liberty@gmail.com>
2012-07-14 13:45:50 -0500

    guard, rspec, capybara and growl all working together TODO: add spork

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,16 +2,18 @@ require 'spec_helper'
 
 describe "Static pages" do
   
+  let(:base_title) { "Sample App" }
+  
   describe "Home page" do
     
     it "should have the content 'Sample App' " do
       visit '/static_pages/home'
-      page.should have_content('Sample App')      
+      page.should have_selector('h1', :text => 'Sample App')      
     end
     
     it "should have the right title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Sample App | Home")
+      page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
 
@@ -19,12 +21,12 @@ describe "Static pages" do
     
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
     end
     
     it "should have the right title" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => "Sample App | Help")
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
   
@@ -32,12 +34,12 @@ describe "Static pages" do
     
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', :text => 'About Us')
     end
     
     it "should have the right title" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => "Sample App | About Us")
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end
 end
